6ad9e56a60893a036e0934ccebfa46deffa27005
Salim66 <salimhasanriad8@gmail.com>
2022-03-07 20:08:19 +0600

    Add Staff Data into JSON Server

--- src/Components/StaffCreate/StaffCreate.jsx
+++ src/Components/StaffCreate/StaffCreate.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card, Col, Container, Form, Row, Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './StaffCreate.css';
 
 const StaffCreate = () => {
+
+    const [input, setInput] = useState({
+        name: '',
+        job: '',
+        photo: ''
+    });
+
+    function handleInputVal(e){
+        
+        setInput({
+            ...input,
+            [e.target.name] : e.target.value
+        });
+
+    }
+
+    let handleSubmitForm = (e) => {
+        e.preventDefault();
+
+        if(input.name == '' && input.job == '' && input.photo == ''){
+
+            alert('All fields are required!');
+            
+        }else {
+
+            axios.post('http://localhost:3000/staff', input);
+
+            setInput({
+                name: '',
+                job: '',
+                photo: ''
+            });
+
+        }
+
+        
+
+    }
+
   return (
     <section className="all-staff">
         <Container>
@@ -11,25 +51,25 @@ const StaffCreate = () => {
                 <Col md={8}>
                     <Card>
                         <Card.Header className='d-flex justify-content-between'>
-                            <h3>Create Staff</h3>
+                            <h3>Create New Staff</h3>
                             <Link to='/' className='btn btn-primary'>All staff</Link>
                         </Card.Header>
                         <Card.Body>
-                            <Form>
+                            <Form onSubmit={ handleSubmitForm } method='POST' autoComplete='off'>
                                 <Form.Group>
                                     <Form.Label>Name</Form.Label>
-                                    <Form.Control  />
+                                    <Form.Control name='name' value={ input.name } onChange={ (e) => handleInputVal(e) } />
                                 </Form.Group>
                                 <Form.Group>
                                     <Form.Label>Job</Form.Label>
-                                    <Form.Control  />
+                                    <Form.Control name='job' value={ input.job } onChange={ (e) => handleInputVal(e) } />
                                 </Form.Group>
                                 <Form.Group>
                                     <Form.Label>Photo</Form.Label>
-                                    <Form.Control  />
+                                    <Form.Control name='photo' value={ input.photo } onChange={ (e) => handleInputVal(e) } />
                                 </Form.Group><br />
                                 <Form.Group>
-                                    <Button className='btn btn-primary btn-sm'>Add now</Button>
+                                    <Button type='submit' className='btn btn-primary btn-sm'>Add now</Button>
                                 </Form.Group>
                             </Form>
                         </Card.Body>
